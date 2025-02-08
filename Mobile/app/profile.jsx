800edf88aa5a6e517e2c2d4bf830f2a0b4fe3a4b
Pavaniupeksha <pavani.20222119@iit.ac.lk>
2025-02-09 00:58:33 +0530

    Profile Page UI in progress

--- Mobile/app/profile.jsx
+++ Mobile/app/profile.jsx
@@ -1,13 +1,218 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import ColorList from '@/components/ColorList'
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { MaterialIcons } from "@expo/vector-icons";
+import { COLORS, images, FONTS, SIZES } from "../constants";
 
 const Profile = () => {
   return (
-    <View>
-      <ColorList color='#4f46e5'/>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      <StatusBar backgroundColor={COLORS.gray} />
 
-export default Profile
+      {/* Cover Image */}
+      <View style={{ width: "100%" }}>
+        <Image
+          source={images.cover}
+          resizeMode="cover"
+          style={{
+            height: 228,
+            width: "100%",
+          }}
+        />
+      </View>
+
+      {/* Profile Section */}
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <Image
+          source={images.profile}
+          resizeMode="contain"
+          style={{
+            height: 155,
+            width: 155,
+            borderRadius: 999,
+            borderColor: COLORS.primary,
+            borderWidth: 2,
+            marginTop: -90,
+          }}
+        />
+
+        <Text
+          style={{
+            ...FONTS.h3,
+            color: COLORS.primary,
+            marginVertical: 8,
+          }}
+        >
+          Melissa Peters
+        </Text>
+        <Text
+          style={{
+            color: COLORS.black,
+            ...FONTS.body4,
+          }}
+        >
+          Interior Designer
+        </Text>
+
+        {/* Location */}
+        <View
+          style={{
+            flexDirection: "row",
+            marginVertical: 6,
+            alignItems: "center",
+          }}
+        >
+          <MaterialIcons name="location-on" size={24} color="black" />
+          <Text
+            style={{
+              ...FONTS.body4,
+              marginLeft: 4,
+            }}
+          >
+            Lagos, Nigeria
+          </Text>
+        </View>
+
+        {/* Stats */}
+        <View
+          style={{
+            flex: 1,
+            paddingVertical: 8,
+            flexDirection: "row",
+          }}
+        >
+          {/* Followers */}
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginHorizontal: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h2,
+                color: COLORS.primary,
+              }}
+            >
+              122
+            </Text>
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.primary,
+              }}
+            >
+              Followers
+            </Text>
+          </View>
+
+          {/* Followings */}
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginHorizontal: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h2,
+                color: COLORS.primary,
+              }}
+            >
+              67
+            </Text>
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.primary,
+              }}
+            >
+              Followings
+            </Text>
+          </View>
+
+          {/* Likes */}
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginHorizontal: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h2,
+                color: COLORS.primary,
+              }}
+            >
+              77K
+            </Text>
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.primary,
+              }}
+            >
+              Likes
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      {/* Buttons */}
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <TouchableOpacity
+          style={{
+            width: 124,
+            height: 36,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: COLORS.primary,
+            borderRadius: 10,
+            marginHorizontal: SIZES.padding * 2,
+          }}
+        >
+          <Text
+            style={{
+              ...FONTS.body4,
+              color: COLORS.white,
+            }}
+          >
+            Edit Profile
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: 124,
+            height: 36,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: COLORS.primary,
+            borderRadius: 10,
+            marginHorizontal: SIZES.padding * 2,
+          }}
+        >
+          <Text
+            style={{
+              ...FONTS.body4,
+              color: COLORS.white,
+            }}
+          >
+            Add Friend
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
