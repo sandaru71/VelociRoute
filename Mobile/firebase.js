ded7fe8d780728acacc0dbb72f52ac4ced5e5404
binuki121 <binuki.20230154@iit.ac.lk>
2025-02-22 11:32:32 +0530

    authentication in progress

--- Mobile/firebase.js
+++ Mobile/firebase.js
@@ -1,32 +1,22 @@
-// Import the functions you need from the SDKs
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDw8THdsPpcK67x9YOuNCPoJfCDnwuI1Mw",
   authDomain: "velociroute-4e6a8.firebaseapp.com",
+  databaseURL: "https://velociroute-4e6a8-default-rtdb.firebaseio.com",
   projectId: "velociroute-4e6a8",
-  storageBucket: "velociroute-4e6a8.appspot.com", // Fixed incorrect storage bucket URL
+  storageBucket: "velociroute-4e6a8.appspot.com",
   messagingSenderId: "809760547860",
-  appId: "1:809760547860:web:65ad36653556582155f301",
+  appId: "1:809760547860:web:65ad36653556582155f301"
 };
 
-let firebaseApp = null;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// Function to initialize Firebase
-export const getFirebaseApp = () => {
-  if (!firebaseApp) {
-    firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
-    // Initialize Firebase Authentication with Async Storage
-    initializeAuth(firebaseApp, {
-      persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-    });
-  }
-  return firebaseApp;
-};
-
-// Export Firebase Authentication instance
-export const auth = getAuth(getFirebaseApp());
+export { auth };
