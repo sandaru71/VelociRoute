570ac387d0327cada7dc50452705a1ed41ce9924
binuki121 <binuki.20230154@iit.ac.lk>
2025-02-23 16:46:35 +0530

    minor changes

---
+++ Mobile/app/(tabs)/Start.jsx
@@ -0,0 +1,120 @@
+import { useNavigation } from '@react-navigation/native'; // Changed import
+import { Image, Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+
+const Start = () => {
+  const navigation = useNavigation(); // Hook to access navigation
+  
+  const handleLogin = () => {
+    navigation.navigate('Login'); // Navigates to the Login screen
+  };
+  const handleSignUp = () => {
+    navigation.navigate('SignUp'); // Navigates to the SignUpscreen
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Image
+        source={require('../../assets/images/cycling1.jpg')}
+        style={styles.cycling1}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Welcome to VelociRoute</Text>
+      <Text style={styles.subtitle}>
+      
+
+Plan your perfect ride, hike, or exploration with AI-powered route optimization. 
+
+🌍 Discover. 🚵‍♀️ Ride. 📍 Explore.
+
+Let’s get started! 🚀
+      </Text>
+      <View style={styles.buttonContainer}>
+        {/* Login Button */}
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        {/* Sign-up Button */}
+        <TouchableOpacity style={styles.signupButton}onPress={handleSignUp}>
+          <Text style={styles.signupButtonText}>Sign-up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Start;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
+  },
+  logo: {
+    width: 150,
+    height: 100,
+    marginBottom: 0,
+  },
+  cycling1: {
+    width: 2000,
+    height: 300,
+    marginTop: 0,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+    marginTop: 50,
+    textAlign:'center',
+  },
+  subtitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 80,
+    borderWidth: 2,
+    borderColor: '#FEBE15',
+    width: '80%',
+    height: '8%',
+    borderRadius: 100,
+  },
+  loginButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FEBE15',
+    padding: 10,
+    borderRadius: 100,
+    width: '50%',
+  },
+  loginButtonText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  signupButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+    borderRadius: 100,
+    width: '50%',
+  },
+  signupButtonText: {
+    color: '#031C30',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
