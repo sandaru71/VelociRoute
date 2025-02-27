277df2398757bae6c4cf38e33aab014b2aa3db61
binuki121 <binuki.20230154@iit.ac.lk>
2025-02-27 11:54:29 +0530

    expo router issue resolved.

--- Mobile/babel.config.js
+++ Mobile/babel.config.js
@@ -1,10 +1,6 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ["babel-preset-expo"],
-      plugins: [
-        ["module:react-native-dotenv"]
-      ],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo']
   };
-  
+};
