f5f6e228945a641390763201c41d4e20c6ae4dd0
binuki121 <binuki.20230154@iit.ac.lk>
2025-03-01 14:36:06 +0530

    firebase changes

--- Mobile/firebase/config.js
+++ Mobile/firebase/config.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // You'll need to replace these with your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAhSJ1H-oPyhV3n2a0oJFqro8ITZecn7Ro",
-  authDomain: "velociroute-ed68f.firebaseapp.com",
-  projectId: "velociroute-ed68f",
-  storageBucket: "velociroute-ed68f.firebasestorage.app",
-  messagingSenderId: "684138177175",
-  appId: "1:684138177175:web:fc3abbc0e6215705eb7fd2",
-  measurementId: "G-DX86VWRLHL"
+  apiKey: "AIzaSyD6LmWo1nrykNZR-RQM2kiLWifsW959gGE",
+  authDomain: "velociroute-5009e.firebaseapp.com",
+  projectId: "velociroute-5009e",
+  storageBucket: "velociroute-5009e.firebasestorage.app",
+  messagingSenderId: "475210861437",
+  appId: "1:475210861437:web:f0dba7fae9f8900737da14",
+  measurementId: "G-6KLF2E5HKB"
 };
 
 // Initialize Firebase
