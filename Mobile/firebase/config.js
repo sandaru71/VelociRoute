9f65638b8506134fedb23f3482e9c33927c5e5fd
Sandaru <ssandarus71@gmail.com>
2025-03-01 22:35:26 +0530

    firebase configuration changed

--- Mobile/firebase/config.js
+++ Mobile/firebase/config.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // You'll need to replace these with your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTxC8RDOO4V5QjDjK6WhhDJHjRHcbB6c0",
-  authDomain: "velociroute-sdgp-1e385.firebaseapp.com",
-  projectId: "velociroute-sdgp-1e385",
-  storageBucket: "velociroute-sdgp-1e385.firebasestorage.app",
-  messagingSenderId: "525855705120",
-  appId: "1:525855705120:web:fbbf58d0d3ccf43122d445",
-  measurementId: "G-46FZ36WCYQ"
+  apiKey: "AIzaSyD6LmWo1nrykNZR-RQM2kiLWifsW959gGE",
+  authDomain: "velociroute-5009e.firebaseapp.com",
+  projectId: "velociroute-5009e",
+  storageBucket: "velociroute-5009e.firebasestorage.app",
+  messagingSenderId: "475210861437",
+  appId: "1:475210861437:web:f0dba7fae9f8900737da14",
+  measurementId: "G-6KLF2E5HKB"
 };
 
 // Initialize Firebase
