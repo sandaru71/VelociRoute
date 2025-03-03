aa76488378aec3ebb412a498a64b6277dd21aee3
binuki121 <binuki.20230154@iit.ac.lk>
2025-03-04 04:02:24 +0530

    weather api integration successful

--- Mobile/app/_layout.jsx
+++ Mobile/app/_layout.jsx
@@ -1,14 +1,25 @@
 import { View } from 'react-native';
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
+import { PaperProvider } from 'react-native-paper';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(app)" options={{ headerShown: false }} />
-      </Stack>
+      <PaperProvider>
+        <Stack 
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#f5f5f5',
+            },
+            headerShadowVisible: false,
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
+        </Stack>
+      </PaperProvider>
     </AuthProvider>
   );
 }
