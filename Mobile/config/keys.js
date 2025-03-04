f9514654c76759dc8ea424edf024a509539dd94d
Sandaru <ssandarus71@gmail.com>
2025-03-04 06:01:56 +0530

    minor changes

--- Mobile/config/keys.js
+++ Mobile/config/keys.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const TOMORROW_API_KEY = 't47SOxYc2a8zpEuIliQbvwFGkRdWCoZg'; // Replace with your actual API key
-=======
 export const TOMORROW_API_KEY = 't47SOxYc2a8zpEuIliQbvwFGkRdWCoZg'; 
->>>>>>> aa76488378aec3ebb412a498a64b6277dd21aee3
